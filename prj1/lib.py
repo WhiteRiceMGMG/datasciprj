f0caad4e9a441140b9ea737a48c5f87c8c869106
ogawa-vaio <3223mq118@gmail.com>
2025-10-21 20:39:21 +0900

    numpy

---
+++ prj1/lib.py
@@ -0,0 +1,19 @@
+import numpy as np
+import numpy.random as random
+import scipy as sp
+import pandas as pd
+from pandas import Series, DataFrame
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+#np.set_printoptions(precision=3, suppress=True)
+
+#numpyについて
+data = np.array([9, 3, 5, 1, 2, 6, 8, 7, 4])
+data
+
+
+
+
