66ce2e0a894220d0e0c589991c86bb61af7c7402
ogawa-vaio <3223mq118@gmail.com>
2025-11-02 20:17:16 +0900

    newton

---
+++ prj1/prac1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import numpy.random as random
+import scipy as sp
+import pandas as pd
+from pandas import Series, DataFrame
+
+import scipy.linalg as linalg
+from scipy.optimize import minimize_scalar
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+from scipy.optimize import newton
+
+#2-1 1から50までの自然数の和．np.arrayで1から50までの配列を作り，総和を求める
+natural_array = np.arange(1,51)
+print(natural_array.sum())
+
+#2-2 標準正規分布に従う乱数を１０個発生させる．また，最小値，最大値，合計を求める
+random.seed(0)
+random_data = random.randn(10)
+print('min:', random_data.min())
+print('max:', random_data.max())
+print('sum:', random_data.sum())
+
+#2-3 要素がすべて3の5x5の行列を作成し，その行列を2乗する計算を実施
+three_array = 3 * np.ones((5,5), dtype = np.float64)
+three_array = three_array ** 2
+print(three_array)
+
+#2-4 次の行列式を求める. [[1,2,3],[1,3,2],[3,1,2]]
+matrix = np.array([[1,2,3],[1,3,2],[3,1,2]])
+matrix_det = linalg.det(matrix)
+print(matrix_det)
+
+#2-5 上の行列について，逆行列と固有値と固有ベクトルを求める
+matrix_inv = linalg.inv(matrix)
+eig_value, eig_vector = linalg.eig(matrix)
+print(matrix_inv)
+print(eig_value)
+print(eig_vector)
+
+#2-6 次の関数の解をニュートン法で求める． f(x) = x ** 3 + 2 * x + 1
+def function(x):
+    return (x ** 3 + 2 * x + 1)
+
+print(newton(function,0))
+
