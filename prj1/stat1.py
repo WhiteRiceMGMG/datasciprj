b39635fee57aac93a0c9abcb4a3653902b3424f2
ogawa-vaio <3223mq118@gmail.com>
2025-11-02 12:30:19 +0900

    add

--- prj1/stat1.py
+++ prj1/stat1.py
@@ -7,3 +7,12 @@ from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
+
+student_data_math = pd.read_csv('chap3/student-mat.csv', sep=';')
+#print(student_data_math.head(10))
+
+print(student_data_math.info())
+
+
+
+
