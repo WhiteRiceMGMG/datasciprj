e6cb7145135b29abf4c33c004210b8f28ce38176
ogawa-vaio <3223mq118@gmail.com>
2025-11-02 17:36:34 +0900

    stat

--- prj1/stat4.py
+++ prj1/stat4.py
@@ -10,15 +10,25 @@ import seaborn as sns
 
 student_data_math = pd.read_csv('chap3/student-mat.csv', sep=';')
 
-plt.boxplot(student_data_math['G1'])
-plt.grid(True)
+#plt.boxplot(student_data_math['G1'])
+#plt.grid(True)
 #plt.show()
 
-plt.boxplot(student_data_math['absences'])
-plt.grid(True)
+#plt.boxplot(student_data_math['absences'])
+#plt.grid(True)
 #plt.show()
 
-plt.boxplot([student_data_math['G1'], student_data_math['G2'], student_data_math['G3']])
+#plt.boxplot([student_data_math['G1'], student_data_math['G2'], student_data_math['G3']])
+#plt.grid(True)
+#plt.show()
+
+#print(student_data_math['absences'].std(ddof=0) / student_data_math['absences'].mean())
+
+#print(student_data_math.std(ddof=0) / student_data_math.mean())
+
+plt.plot(student_data_math['G1'], student_data_math['G3'], 'o')
+plt.ylabel('G3 grade')
+plt.xlabel('G1 grade')
 plt.grid(True)
 plt.show()
 
