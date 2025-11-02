81c71e30763504e8fc62da5d79085d2bc402ed78
ogawa-vaio <3223mq118@gmail.com>
2025-11-02 16:00:19 +0900

    stat2

---
+++ prj1/stat2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import numpy.random as random
+import scipy as sp
+import pandas as pd
+from pandas import Series, DataFrame
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+
+student_data_math = pd.read_csv('chap3/student-mat.csv', sep=';')
+
+plt.hist(student_data_math['absences'])
+
+plt.xlabel('absences')
+plt.ylabel('count')
+
+plt.grid(True)
+
+plt.show()
