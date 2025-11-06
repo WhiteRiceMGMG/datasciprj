505f5b9156080a8b36f76c1b217fa8fa6371fa87
ogawa-vaio <3223mq118@gmail.com>
2025-11-06 22:05:20 +0900

    a

--- prj1/prac3.py
+++ prj1/prac3.py
@@ -20,7 +20,7 @@ print(student_por_data.head())
 print(student_por_data.info())
 print(student_por_data['school'].head())
 print(student_por_data['absences'].head())
-print(student_por_data['absences'].descrive())
+print(student_por_data['absences'].describe())
 
 #*************************************************************************************#
 #3-2 数学のデータとポルトガル語のデータをマージ．                                     #
@@ -28,5 +28,25 @@ print(student_por_data['absences'].descrive())
 #  'Fjob', 'reason', 'nursery', 'internet']                                           #
 # suffixes=('_math', '_por')                                                          #
 #*************************************************************************************#
+student_mat_data = pd.read_csv('chap3/student-mat.csv', sep=';')
+join_keys =  ['school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu', 'Mjob',
+              'Fjob', 'reason', 'nursery', 'internet']
+student_merge = pd.merge(student_por_data,
+         student_mat_data,
+         on=join_keys,
+         suffixes=('_math', '_por'))
+print(student_merge.head())
 
+#**************************************************************************************#
+#3-3 マージしたデータについて，いくつかの変数をピックアップして散布図とヒストグラム描写#
+#**************************************************************************************#
+plt.figure(figsize=(10, 6))
+plt.scatter(student_merge['G3_math'], student_merge['G3_por'])
+plt.grid(True)
+plt.show()
 
+plt.hist(student_merge['G3_math'], bins=10)
+plt.subplot(1, 2, 2)
+plt.hist(student_merge['G3_por'], bins=10)
+plt.tight_layout()
+plt.show()
